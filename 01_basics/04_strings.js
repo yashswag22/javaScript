211e0e4ce3f7afa3218efc75992d0cf239dd5295
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2024-01-09 21:40:01 +0000

    in this we learnt the methods/function we apply in a string

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,12 @@
+const gamename = "iamyourgame";
+//console.log(gamename[0]);
+//console.log(gamename.toUpperCase());
+
+const newname = gamename.slice(-7, 0); -                    -
+console.log(newname);
+
+
+
+
+
+
