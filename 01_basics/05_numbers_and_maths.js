94317a1b8d90ee26402a768142b6caf01fb7d2eb
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2024-01-11 20:31:40 +0000

    in this we learnt the use of math libraries

--- 01_basics/05_numbers_and_maths.js
+++ 01_basics/05_numbers_and_maths.js
@@ -13,3 +13,22 @@ const otherNumber = 123.8966
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random()); // it gives random the random values betweeen 0 to 1.
+console.log((Math.random()*10) + 1); // multiplying with 10 increases the range from 0 to 10 and adding 1 makes the range 1 to 10
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // this is the method to set range of random nmbrs from min to max
